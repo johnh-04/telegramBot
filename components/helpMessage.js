c5153634e4b31e7c57f9029d31e7aa77a921ce2a
Vanni Martello <62107576+johnh-04@users.noreply.github.com>
2025-04-24 15:54:11 +0200

    telegramBot v1.1.1
    markdown

--- components/helpMessage.js
+++ components/helpMessage.js
@@ -1,21 +1,21 @@
 const helpMessage = `🤖 Ecco cosa posso fare per te:
 
-📝 /text __testo__
+📝 /text _testo_
 Ti restituisco il messaggio che hai scritto.
 
-💱 /eur __numero__
+💱 /eur _numero_
 Converto da *Euro* a *Dollari*.
 
-💵 /usd __numero__
+💵 /usd _numero_
 Converto da *Dollari* a *Euro*.
 
-📨 /spam __numero__
+📨 /spam _numero_
 Invio un messaggio ripetuto più volte (massimo 10).
 
-🌤️ /weather __città__
+🌤️ /weather _città_
 Ti dico il meteo attuale nella città indicata.
 
-🔍 /google __ricerca__
+🔍 /google _ricerca_
 Eseguo una ricerca su Google con le parole fornite.
 
 ℹ️ /help
